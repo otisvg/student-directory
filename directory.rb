b3942030c514a5614bbb883780783ce76ee27bb9
Otis <otisvickersgraver@gmail.com>
2020-09-26 11:08:11 +0100

    refactoring using instance variable for students array, split interactive menu into several methods for readability

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+@students = []
 
 def input_students
   puts "Please enter the names of the students"
@@ -13,55 +14,62 @@ def input_students
     cohort = "november"
   end
   while !name.empty? && !cohort.empty? do
-    students << {name: name, cohort: :"#{cohort}"}
-    if students.length == 1
-      puts "Now we have #{students.count} great student"
+    @students << {name: name, cohort: :"#{cohort}"}
+    if @students.length == 1
+      puts "Now we have #{@students.count} great student"
     else
-      puts "Now we have #{students.count} great students"
+      puts "Now we have #{@students.count} great students"
     end
     # get another name from the user
     name = gets.chomp
     cohort = gets.chomp
   end
-  # return the array of students
-  students
 end
 # and then print them
 def print_header
   puts "The students of Villains Academy".center(75, " ")
   puts "-------------".center(75, " ")
 end
-def print(students)
-  if students.length >= 1
-    students.each do |student|
+def print
+  if @students.length >= 1
+    @students.each do |student|
       puts "#{student[:name]} (#{student[:cohort]} cohort)".center(75, " ")
     end
   end
 end
-def print_footer(students)
-  puts "Overall we have #{students.count} great students"
+def print_footer
+  puts "Overall we have #{@students.count} great students"
 end
 
-def interactive_menu
-  students = []
-  loop do
-    #1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print
+  print_footer
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
-      puts "I don't know what you meant, please try again"
-    end
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 
